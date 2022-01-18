a8959383481badf417da2cec0ed1c0b0a6c06279
Alex Curran <alexcurran.va@gmail.com>
2022-01-18 14:57:08 -0600

    Add types to params and returns for funcs

--- sorting/merge-sort.js
+++ sorting/merge-sort.js
@@ -1,15 +1,62 @@
+// /**
+//  * Solution 1
+//  * 
+//  * @param {number[]} numsArr 
+//  * @returns {number[]}
+//  */
+// function mergeSort(numsArr) {
+//     if (numsArr.length <= 1) return numsArr;
+
+//     const sortedArr = [...numsArr];
+//     const length = sortedArr.length;
+//     const middle = Math.floor(length / 2);
+//     const left = sortedArr.slice(0, middle);
+//     const right = sortedArr.slice(middle);
+
+//     return merge(mergeSort(left), mergeSort(right));
+// }
+
+// /**
+//  * @param {number[]} left 
+//  * @param {number[]} right 
+//  * @returns {number[]}
+//  */
+// function merge(left, right) {
+//     let results = [];
+
+//     while (left.length && right.length) {
+//         if (left[0] <= right[0]) {
+//             results.push(left.shift());
+//         } else {
+//             results.push(right.shift());
+//         }
+//     }
+
+//     return [...results, ...left, ...right];
+// }
+
+/**
+ * Solution 2 -- slight variation
+ * 
+ * @param {number[]} numsArr 
+ * @returns {number[]}
+ */
 function mergeSort(numsArr) {
     if (numsArr.length <= 1) return numsArr;
 
     const sortedArr = [...numsArr];
-    const length = sortedArr.length;
-    const middle = Math.floor(length / 2);
+    const middle = Math.floor(sortedArr.length / 2);
     const left = sortedArr.slice(0, middle);
     const right = sortedArr.slice(middle);
 
     return merge(mergeSort(left), mergeSort(right));
 }
 
+/**
+ * @param {number[]} left 
+ * @param {number[]} right 
+ * @returns {number[]}
+ */
 function merge(left, right) {
     let results = [];
 
