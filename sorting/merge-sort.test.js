07f94ab6910333b205b33347c0a49af94ec02711
Alex Curran <alexcurran.va@gmail.com>
2022-01-18 14:52:07 -0600

    Add test, remove console.log() statements from merge-sort.js

--- sorting/merge-sort.test.js
+++ sorting/merge-sort.test.js
@@ -5,7 +5,7 @@ describe("merge sort", () => {
         expect(mergeSort([51, 2, 7, 5, 48, 101])).toEqual([2, 5, 7, 48, 51, 101]);
     });
 
-    // test("returns a sorted array in ascending order", () => {
-
-    // });
+    test("returns a sorted array in ascending order", () => {
+        expect(mergeSort([32, 17, 56, 3])).toEqual([3, 17, 32, 56]);
+    });
 });
