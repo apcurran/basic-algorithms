3f434322788a10bf8b091647172df63d102ccfdf
Alex Curran <alexcurran.va@gmail.com>
2022-01-18 15:29:15 -0600

    Working alt solution

--- sorting/merge-sort.js
+++ sorting/merge-sort.js
@@ -1,43 +1,5 @@
-/**
- * Solution 1
- * 
- * @param {number[]} numsArr 
- * @returns {number[]}
- */
-function mergeSort(numsArr) {
-    if (numsArr.length <= 1) return numsArr;
-
-    const middle = Math.floor(numsArr.length / 2);
-    const left = numsArr.slice(0, middle);
-    const right = numsArr.slice(middle);
-
-    return merge(
-        mergeSort(left),
-        mergeSort(right)
-    );
-}
-
-/**
- * @param {number[]} left 
- * @param {number[]} right 
- * @returns {number[]}
- */
-function merge(left, right) {
-    let results = [];
-
-    while (left.length && right.length) {
-        if (left[0] <= right[0]) {
-            results.push(left.shift());
-        } else {
-            results.push(right.shift());
-        }
-    }
-
-    return [...results, ...left, ...right];
-}
-
 // /**
-//  * Solution 2 -- slight variation
+//  * Solution 1
 //  * 
 //  * @param {number[]} numsArr 
 //  * @returns {number[]}
@@ -45,13 +7,13 @@ function merge(left, right) {
 // function mergeSort(numsArr) {
 //     if (numsArr.length <= 1) return numsArr;
 
-//     const middleIndex = Math.floor(numsArr.length / 2);
-//     const leftArr = numsArr.slice(0, middleIndex);
-//     const rightArr = numsArr.slice(middleIndex);
+//     const middle = Math.floor(numsArr.length / 2);
+//     const left = numsArr.slice(0, middle);
+//     const right = numsArr.slice(middle);
 
 //     return merge(
-//         mergeSort(leftArr),
-//         mergeSort(rightArr)
+//         mergeSort(left),
+//         mergeSort(right)
 //     );
 // }
 
@@ -73,5 +35,50 @@ function merge(left, right) {
 
 //     return [...results, ...left, ...right];
 // }
+
+/**
+ * Solution 2 -- slight variation
+ * 
+ * @param {number[]} numsArr 
+ * @returns {number[]}
+ */
+function mergeSort(numsArr) {
+    if (numsArr.length <= 1) return numsArr;
+
+    const middleIndex = Math.floor(numsArr.length / 2);
+    const leftArr = numsArr.slice(0, middleIndex);
+    const rightArr = numsArr.slice(middleIndex);
+
+    return merge(
+        mergeSort(leftArr),
+        mergeSort(rightArr)
+    );
+}
+
+/**
+ * @param {number[]} leftArr - sorted arr
+ * @param {number[]} rightArr - sorted arr
+ * @returns {number[]}
+ */
+function merge(leftArr, rightArr) {
+    let resultArr = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+    
+    while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
+        const leftElem = leftArr[leftIndex];
+        const rightElem = rightArr[rightIndex];
+
+        if (leftElem < rightElem) {
+            resultArr.push(leftElem);
+            leftIndex++;
+        } else {
+            resultArr.push(rightElem);
+            rightIndex++;
+        }
+    }
+
+    return [...resultArr, ...leftArr.slice(leftIndex), ...rightArr.slice(rightIndex)];
+}
 
 module.exports = { mergeSort };
