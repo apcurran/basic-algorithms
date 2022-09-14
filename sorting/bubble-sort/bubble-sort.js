5b9bd9f5fbe0035289bbf2f303d1289c3035993d
Alex Curran <alexcurran.va@gmail.com>
2022-09-14 15:34:44 -0500

    Add alt way of swapping and iterating

--- sorting/bubble-sort/bubble-sort.js
+++ sorting/bubble-sort/bubble-sort.js
@@ -1,6 +1,10 @@
 "use strict";
 
 /**
+ * solution 1
+ * time: O(n^2)
+ * space: O(1)
+ * 
  * @param {number[]} arr 
  * @returns {number[]}
  */
@@ -15,5 +19,28 @@ function bubbleSort(arr) {
 
     return arr;
 }
+
+// /**
+//  * solution 2
+//  * time: O(n^2)
+//  * space: O(1)
+//  * 
+//  * @param {number[]} arr 
+//  * @returns {number[]}
+//  */
+//  function bubbleSort(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = 0; j < arr.length - 1 - i; j++) {
+//             if (arr[j] > arr[j + 1]) {
+//                 // swap
+//                 const temp = arr[j + 1];
+//                 arr[j + 1] = arr[j];
+//                 arr[j] = temp;
+//             }
+//         }
+//     }
+
+//     return arr;
+// }
 
 module.exports = { bubbleSort };
