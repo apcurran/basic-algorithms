255bb63c46fa51ca1dcda98d4921b1e3ee420eb9
Alex Curran <alexcurran.va@gmail.com>
2022-09-20 15:07:40 -0500

    Revise var namings and update midIndex calculation

--- searching/binary-search.js
+++ searching/binary-search.js
@@ -1,58 +1,59 @@
 "use strict";
 
-/**
- * solution 1 -- iterative
- * time: O(log n)
- * space: O(1)
- *
- * @param {number[]} arr 
- * @param {number} target 
- * @returns {number}
- */
-function binarySearch(arr, target) {
-    let low = 0;
-    let high = arr.length;
-
-    while (low < high) {
-        const mid = Math.floor(low + (high - low) / 2);
-        const val = arr[mid];
-
-        if (val === target) {
-            return mid;
-        } else if (val > target) {
-            // search left half -- discard right
-            high = mid;
-        } else {
-            // search right half -- discard left
-            low = mid + 1;
-        }
-    }
-
-    return -1;
-}
-
 // /**
-//  * solution 2 -- recursive
+//  * solution 1 -- iterative
 //  * time: O(log n)
-//  * space: O(log n)
-//  * 
+//  * space: O(1)
+//  *
 //  * @param {number[]} arr 
 //  * @param {number} target 
 //  * @returns {number}
 //  */
-// function binarySearch(arr, target, startIndex = 0, endIndex = arr.length - 1) {
-//     // first base case
-//     if (startIndex > endIndex) return -1;
+// function binarySearch(arr, target) {
+//     let low = 0;
+//     let high = arr.length;
 
-//     const midPoint = Math.floor((startIndex + endIndex) / 2);
+//     while (low < high) {
+//         const mid = Math.floor(low + (high - low) / 2);
+//         const val = arr[mid];
 
-//     if (arr[midPoint] === target) {
-//         return midPoint;   
-//     } else if (arr[midPoint] < target) {
-//         return binarySearch(arr, target, midPoint + 1, endIndex);
-//     } else if (arr[midPoint] > target) {
-//         return binarySearch(arr, target, startIndex, midPoint - 1);
+//         if (val === target) {
+//             return mid;
+//         } else if (val > target) {
+//             // search left half -- discard right
+//             high = mid;
+//         } else {
+//             // search right half -- discard left
+//             low = mid + 1;
+//         }
 //     }
+
+//     return -1;
 // }
+
+/**
+ * solution 2 -- recursive
+ * time: O(log n)
+ * space: O(log n)
+ * 
+ * @param {number[]} arr 
+ * @param {number} target 
+ * @returns {number}
+ */
+function binarySearch(arr, target, lowIndex = 0, highIndex = arr.length - 1) {
+    // first base case
+    if (lowIndex > highIndex) return -1;
+
+    const midIndex = lowIndex + Math.floor((highIndex - lowIndex) / 2);
+    const midVal = arr[midIndex];
+
+    if (midVal === target) {
+        return midIndex;   
+    } else if (midVal < target) {
+        return binarySearch(arr, target, midIndex + 1, highIndex);
+    } else if (midVal > target) {
+        return binarySearch(arr, target, lowIndex, midIndex - 1);
+    }
+}
 
 module.exports = { binarySearch };
