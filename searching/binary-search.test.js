6caf9ecf05f7ce4557ff0b1378e1b2e531d9f1e9
Alex Curran <alexcurran.va@gmail.com>
2023-04-05 14:27:16 -0500

    Add test for bin search

--- searching/binary-search.test.js
+++ searching/binary-search.test.js
@@ -5,6 +5,10 @@ describe("binary search", () => {
     test("returns index of 2 for input target of 12", () => {
         expect(binarySearch([1, 5, 12, 53, 74, 143], 12)).toBe(2);
     });
+
+    test("returns index of 2 for input target of 12", () => {
+        expect(binarySearch([1, 2, 7, 12, 15], 2)).toBe(1);
+    });
     
     test("returns -1 value if target is not present in the array", () => {
         expect(binarySearch([1, 2, 3, 4], 10)).toBe(-1);
