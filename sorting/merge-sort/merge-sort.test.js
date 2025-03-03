53f9272e418129340863c2bc4d83df2ef1594af5
Alex Curran <alexcurran.va@gmail.com>
2025-03-03 16:02:40 -0600

    Add test for sorting an already sorted array

--- sorting/merge-sort/merge-sort.test.js
+++ sorting/merge-sort/merge-sort.test.js
@@ -8,4 +8,8 @@ describe("merge sort", () => {
     test("returns a sorted array in ascending order", () => {
         expect(mergeSort([32, 17, 56, 3])).toEqual([3, 17, 32, 56]);
     });
+
+    test("sorts an already sorted array", () => {
+        expect(mergeSort([1, 2, 3, 5])).toEqual([1, 2, 3, 5]);
+    });
 });
